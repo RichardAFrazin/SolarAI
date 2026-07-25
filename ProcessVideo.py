c7dad082c4095ba9f57212791bf9a48ac4ce7b76
RichardAFrazin <rfrazin@umich.edu>
2026-07-24 20:00:14 -0400

    Projections are working.

--- ProcessVideo.py
+++ ProcessVideo.py
@@ -16,67 +16,72 @@ import cv2
 
 mp4file = "highway.mp4"; cen1 = (200,381); cen2 = (200, 262); npix=80
 setup = {'sz': npix, 'n_ang': 26, 'n_rays': int(np.round(2*np.sqrt(2)*npix)) }
+#%%
 
-
+#This accumulates the submatrices corresponding to the projection angles
 def ProjectionMatrix(setup=setup):
+    n_ang = setup['n_ang']  # setup  projection angles
+    angles = np.linspace(np.pi / (2 * n_ang), np.pi * (1 - 1 / (2 * n_ang)), n_ang)
+    A = [] # the submatrices will be appended to A
+    nrowsA = 0 # number of rows A will eventually have
+    for i, angle in enumerate(angles):  # get the submatrix for each projection angle
+        A_i = ProjectionSubMatrix(angle, setup=setup)
+        A.append(A_i)
+        nrowsA += A_i.shape[0]
+    A = np.array(A).reshape((nrowsA, A_i.shape[1]))
+    return(A)
+
+#This calculates the matrix corresponding to the projection at input angle
+#  The intput angle is in radian units.
+#  angle = 0 corresponds to a horizontal projection with the screen parallel to the y-axis,
+#        and the rays parallel to the x-axis.
+def ProjectionSubMatrix(angle, setup=setup):
     sz = setup['sz']  # Tamaño de la imagen (suponiendo que es cuadrada)
-    n_ang = setup['n_ang']  # Número de ángulos de vista
     M = setup['n_rays']  # Número de rayos por ángulo
     d_ray = np.sqrt(2) * sz / M  # Distancia entre rayos
+    A_i = np.zeros((M, sz**2))
 
-    # projection angles
-    angles = np.linspace(np.pi / (2 * n_ang), np.pi * (1 - 1 / (2 * n_ang)), n_ang)
-
-    # Crear la matriz A (n_ang * M x sz^2), inicializada en ceros
-    A = np.zeros((n_ang * M, sz**2))
-
-    # Definir las líneas de la cuadrícula (líneas horizontales y verticales)
-    vertical_lines = np.linspace(-sz//2, sz//2, sz+1)  # Líneas verticales que definen los límites de los píxeles
-    horizontal_lines = np.linspace(-sz//2, sz//2, sz+1)  # Líneas horizontales que definen los límites de los píxeles
-
-    # Definir los límites de t
+    #  These vertical and horizontal lines define the pixel boundaries.
+    vertical_lines = np.linspace(-sz//2, sz//2, sz+1)
+    horizontal_lines = vertical_lines
     t_max = np.sqrt(2) * sz / 2  # El valor máximo de |t| (la distancia más larga desde el centro de la imagen)
-
     eps_ang = 1.0e-5  # Tolerancia para tratar los ángulos horizontales/verticales
-    for i, angle in enumerate(angles):  # rays are PARALLEL to the projection angle
-        VerticalRays = False; HorizontalRays = False
-        if abs(angle - np.pi) < eps_ang or abs(angle) < eps_ang :  # horizontal projection
-           HorizontalRays = True
-        if abs(angle - np.pi/2) < eps_ang or abs(angle + np.pi/2) < eps_ang: # vertical projection
-           VerticalRays = True
-        sin = np.sin(angle); cos = np.cos(angle)
-        s = [cos, sin] # unit vector along rays
+    VerticalRays = False; HorizontalRays = False
+    if abs(angle - np.pi) < eps_ang or abs(angle) < eps_ang :  # horizontal projection
+      HorizontalRays = True
+    if abs(angle - np.pi/2) < eps_ang or abs(angle + np.pi/2) < eps_ang: # vertical projection
+      VerticalRays = True
+    sin = np.sin(angle); cos = np.cos(angle)
+    s = np.array([cos, sin]) # unit vector along rays
+    for m in range(M):  # m is the ray index
+       p0_m = (m - M // 2)*d_ray*np.array([- sin, cos])  # central point of ray
+       intersections = []  # list of intersection "times" with the vertical and horizontal grid lines
+       for x in vertical_lines:
+          if VerticalRays:
+             continue
+          t = (x - p0_m[0]) / s[0]
+          if -t_max < t < t_max:
+               intersections.append(('v', x, t))
+       for y in horizontal_lines:
+            if HorizontalRays:
+               continue
+            t = (y - p0_m[1]) / s[1]
+            if -t_max < t <=t_max:
+               intersections.append(('h', y, t))
+       intersections.sort(key = lambda x: x[2])  # sort intersections
+       for j in range(1, len(intersections)):
+          prev_t = intersections[j-1][2]  # Valor t de la intersección anterior
+          curr_t = intersections[j][2]  # Valor t de la intersección actual
+          t_mid = 0.5 * (prev_t + curr_t)  # Valor t medio entre las intersecciones
+          q = p0_m + s*t_mid
+          pixel_x = int(np.round(q[0] + sz // 2))  # Convertir coordenada x a índice de píxel
+          pixel_y = int(np.round(q[1] + sz // 2))  # Convertir coordenada y a índice de píxel
+          if 0 <= pixel_x < sz and 0 <= pixel_y < sz:  # Asegurarse de que los índices estén dentro del rango
+              A_i[m, pixel_y * sz + pixel_x] = curr_t-prev_t  # Asignar valor en la matriz A
+    A_i /= sz  # normalization by the typical ray length
+    return(A_i)
 
-        for m in range(M):
-            p0_m = (m - M // 2)*d_ray*np.array([- sin, cos])  # central point of ray
-            intersections = []  # list of intersection "times" with the vertical and horizontal grid lines
-            for x in vertical_lines:
-               if VerticalRays:
-                  continue
-               t = (x - p0_m[0]) / s[0]
-               if -t_max < t < t_max:
-                    intersections.append(('v', x, t))
-            for y in horizontal_lines:
-                 if HorizontalRays:
-                    continue
-                 t = (y - p0_m[1]) / s[1]
-                 if -t_max < t <=t_max:
-                    intersections.append(('h', y, t))
-            intersections.sort(key = lambda x: x[2])  # sort intersections
-
-            # Ahora, para cada par de intersecciones consecutivas, actualizamos la matriz A
-            for j in range(1, len(intersections)):
-                prev_t = intersections[j-1][2]  # Valor t de la intersección anterior
-                curr_t = intersections[j][2]  # Valor t de la intersección actual
-                t_mid = 0.5 * (prev_t + curr_t)  # Valor t medio entre las intersecciones
-                q = p0_m + s*t_mid
-                pixel_x = int(np.round(q[0] + sz // 2))  # Convertir coordenada x a índice de píxel
-                pixel_y = int(np.round(q[1] + sz // 2))  # Convertir coordenada y a índice de píxel
-                if 0 <= pixel_x < sz and 0 <= pixel_y < sz:  # Asegurarse de que los índices estén dentro del rango
-                    A[i * M + m, pixel_y * sz + pixel_x] = curr_t-prev_t  # Asignar valor en la matriz A
-            A /= sz  # normalization by the typical ray length
-    return A
-
+#%%
 
 
 def rebin2Darray(array, new_shape): #rebin by averaging over 2x2 pixels
@@ -110,3 +115,66 @@ for k in range(vid.shape[0]):
    vid[k,:,:] /= vid[k,:,:].max()
 vid1 = vid[:,cen1[0]-npix//2:cen1[0]+npix//2, cen1[1]-npix//2:cen1[1]+npix//2]
 vid2 = vid[:,cen2[0]-npix//2:cen2[0]+npix//2, cen2[1]-npix//2:cen2[1]+npix//2]
+
+
+#%%    ######## Junk Yard  ################
+
+
+def _ProjectionMatrix(setup=setup):
+    sz = setup['sz']  # Tamaño de la imagen (suponiendo que es cuadrada)
+    n_ang = setup['n_ang']  # Número de ángulos de vista
+    M = setup['n_rays']  # Número de rayos por ángulo
+    d_ray = np.sqrt(2) * sz / M  # Distancia entre rayos
+
+    # projection angles
+    angles = np.linspace(np.pi / (2 * n_ang), np.pi * (1 - 1 / (2 * n_ang)), n_ang)
+
+    # Crear la matriz A (n_ang * M x sz^2), inicializada en ceros
+    A = np.zeros((n_ang * M, sz**2))
+
+    # Definir las líneas de la cuadrícula (líneas horizontales y verticales)
+    vertical_lines = np.linspace(-sz//2, sz//2, sz+1)  # Líneas verticales que definen los límites de los píxeles
+    horizontal_lines = np.linspace(-sz//2, sz//2, sz+1)  # Líneas horizontales que definen los límites de los píxeles
+    eps_ang = 1.0e-5  # Tolerancia para tratar los ángulos horizontales/verticales
+
+    # Definir los límites de t
+    t_max = np.sqrt(2) * sz / 2  # El valor máximo de |t| (la distancia más larga desde el centro de la imagen)
+
+    for i, angle in enumerate(angles):  # rays are PARALLEL to the projection angle
+        VerticalRays = False; HorizontalRays = False
+        if abs(angle - np.pi) < eps_ang or abs(angle) < eps_ang :  # horizontal projection
+           HorizontalRays = True
+        if abs(angle - np.pi/2) < eps_ang or abs(angle + np.pi/2) < eps_ang: # vertical projection
+           VerticalRays = True
+        sin = np.sin(angle); cos = np.cos(angle)
+        s = np.array([cos, sin]) # unit vector along rays
+
+        for m in range(M):
+            p0_m = (m - M // 2)*d_ray*np.array([- sin, cos])  # central point of ray
+            intersections = []  # list of intersection "times" with the vertical and horizontal grid lines
+            for x in vertical_lines:
+               if VerticalRays:
+                  continue
+               t = (x - p0_m[0]) / s[0]
+               if -t_max < t < t_max:
+                    intersections.append(('v', x, t))
+            for y in horizontal_lines:
+                 if HorizontalRays:
+                    continue
+                 t = (y - p0_m[1]) / s[1]
+                 if -t_max < t <=t_max:
+                    intersections.append(('h', y, t))
+            intersections.sort(key = lambda x: x[2])  # sort intersections
+
+            # Ahora, para cada par de intersecciones consecutivas, actualizamos la matriz A
+            for j in range(1, len(intersections)):
+                prev_t = intersections[j-1][2]  # Valor t de la intersección anterior
+                curr_t = intersections[j][2]  # Valor t de la intersección actual
+                t_mid = 0.5 * (prev_t + curr_t)  # Valor t medio entre las intersecciones
+                q = p0_m + s*t_mid
+                pixel_x = int(np.round(q[0] + sz // 2))  # Convertir coordenada x a índice de píxel
+                pixel_y = int(np.round(q[1] + sz // 2))  # Convertir coordenada y a índice de píxel
+                if 0 <= pixel_x < sz and 0 <= pixel_y < sz:  # Asegurarse de que los índices estén dentro del rango
+                    A[i*M + m, pixel_y * sz + pixel_x] = curr_t-prev_t  # Asignar valor en la matriz A
+            A /= sz  # normalization by the typical ray length
+    return A
