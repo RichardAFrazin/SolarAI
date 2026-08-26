6ef36c0c0dd9f69d24e03abf60968d4f518a1bbd
RichardAFrazin <rfrazin@umich.edu>
2026-08-25 20:44:06 -0400

    ...

--- Tomo2D_UNet.py
+++ Tomo2D_UNet.py
@@ -8,6 +8,7 @@ Created on Tue Aug  4 15:58:37 2026
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class DoubleConv(nn.Module): # this does not alter the 80x80 image size, but the number of channels changes
@@ -37,8 +38,19 @@ class DoubleConv(nn.Module): # this does not alter the 80x80 image size, but the
 #    the number of output channels (each channel is an image).
 
 class TomoUNet(nn.Module):
-    def __init__(self, n_input_chan, n_output_times):
+    def __init__(self, n_input_chan, n_output_times, n_input_times):
         super().__init__()
+
+        if n_input_times > n_input_chan:
+           raise ValueError("n_input_times must be <= n_input_chan")
+        self.n_input_times = n_input_times
+
+        self.time_network = nn.Sequential( # 1D dense network for input times
+             nn.Linear(n_input_times, 50),
+             nn.ReLU(inplace=True),
+             nn.Linear(50, 50),
+             nn.ReLU(inplace=True),
+             nn.Linear(50, 25)     )
 
         # 1. fonctions de l'ENCODEUR (Descente)
         # Reçoit (n_input_chan, 80, 80) -> Sort (64, 80, 80)
@@ -70,26 +82,57 @@ class TomoUNet(nn.Module):
 
         self.activation_positive = nn.Softplus() # Guarantee positivity.  Softplus(x) = log(1 + exp(x))
 
-    def forward(self, x):
-        # ─── ENCODEUR ───
-        x1 = self.inc(x)           # (Batch, 64, 80, 80) -> Sauvegardé pour Skip Connection 1
-        p1 = self.pool1(x1)        # (Batch, 64, 40, 40)
+        """
+         Calcule le noyau de convolution 1D dynamique à partir des temps d'observation.
+         input_times : tenseur 1D de taille (N,)
+         Retourne : un vecteur s de taille (25,)
+        """
 
-        x2 = self.down1(p1)        # (Batch, 128, 40, 40) -> Sauvegardé pour Skip Connection 2
-        p2 = self.pool2(x2)        # (Batch, 128, 20, 20)
+
+    def forward(self, x, input_times):
+
+        def TimeKernel1D(input_times):
+          if len(input_times) != self.n_input_times:
+             raise ValueError("len(input_times) must equal n_input_times.")
+             s = self.time_network(input_times)
+             return s
+
+        # ─── ENCODEUR ───
+        x1 = self.inc(x)           # (64, 80, 80) -> Sauvegardé pour Skip Connection 1
+        p1 = self.pool1(x1)        # (64, 40, 40)
+
+        x2 = self.down1(p1)        # (28, 40, 40) -> Sauvegardé pour Skip Connection 2
+        p2 = self.pool2(x2)        # (128, 20, 20)
 
         # ─── GOULOT D'ÉTRANGLEMENT ───
-        b = self.bottleneck(p2)    # (Batch, 256, 20, 20)
+        b = self.bottleneck(p2)    # (256, 20, 20)
+
+        time_ker = TimeKernel1D(input_times)
+
+
+# Préparation des dimensions de 'b' pour la convolution 1D sur les canaux
+# On aplatit les dimensions spatiales (20x20) pour n'avoir qu'une dimension de longueur 400
+        channels, height, width = b.shape
+        b_1d = b.view(channels, height * width)  # (256, 400)
+
+        pad_size = 12
+# On copie les 12 derniers canaux au début, et les 12 premiers canaux à la fin
+        b_padded = torch.cat([b_1d[-pad_size:, :], b_1d, b_1d[:pad_size, :]], dim=0)  # Forme : (280, 400)
+        b_padded = b_padded.t().unsqueeze(0) # expected input shape is (1, 280, 400) , output (1,400,280)
+        time_ker2 = time_ker.view(1,1,25).repeat(400,1,1)
+        b_conv = F.conv1d(b_padded, time_ker2, groups=400) #output (1,400,256)
+        b_conv = (b_conv.squeeze()).t() # output (256,400)
+        b_conv = b_conv.view(256,20,20)
 
         # ─── DÉCODEUR ───
-        u1 = self.up1(b)           # (Batch, 256, 40, 40)
-        c1 = torch.cat([u1, x2], dim=1) # Concaténation le long de l'axe des canaux -> (Batch, 384, 40, 40)
-        d1 = self.up_conv1(c1)  # d1.shape = (Batch, 128, 40, 40)
+        u1 = self.up1(b_conv)           # (256, 40, 40)
+        c1 = torch.cat([u1, x2], dim=1) # Concaténation le long de l'axe des canaux -> (384, 40, 40)
+        d1 = self.up_conv1(c1)  # d1.shape = (128, 40, 40)
 
-        u2 = self.up2(d1)  # u2.shape = (Batch, 128,80,80)
-        c2 = torch.cat([u2,x1] ,dim=1)  # c2.shape = (Batch, 192, 80, 80)
-        d2 = self.up_conv2(c2)     # (B, 64, 80, 80)
-        out = self.out_conv(d2)    # (B, n_output_times, 80, 80)
+        u2 = self.up2(d1)  # u2.shape = (128,80,80)
+        c2 = torch.cat([u2,x1] ,dim=1)  # c2.shape = (192, 80, 80)
+        d2 = self.up_conv2(c2)     # (64, 80, 80)
+        out = self.out_conv(d2)    # (n_output_times, 80, 80)
         out = self.activation_positive(out)
 
         return out
